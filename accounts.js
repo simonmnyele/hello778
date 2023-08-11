58e0c958b63f0642856d8c04c549e062caa843fa
simonmnyele <55534977+simonmnyele@users.noreply.github.com>
2023-08-12 02:18:09 +0300

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,3 +1,4 @@
 module.exports = [
-    { id: '902926941413453824', user: 'cz_binance' }
+    { id: '902926941413453824', user: 'cz_binance' },
+    { id: '1484112393361891328', user: 'GCRClassic' }
 ]
