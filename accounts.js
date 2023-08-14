440c22c7cc848812cc5304af68706754f39755bd
simonmnyele <55534977+simonmnyele@users.noreply.github.com>
2023-08-14 21:45:08 +0300

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -3,5 +3,6 @@ module.exports = [
     { id: '1484112393361891328', user: 'GCRClassic' },
     { id: '1067927682023915521', user: 'sweatystartup' },
     { id: '104281062', user: 'girdley' },
-    { id: '1658190072666021941', user: 'FrederikFeldt_' } 
+    { id: '1658190072666021941', user: 'FrederikFeldt_' },
+    { id: '1430251685847474180', user: 'flopstofreedom' }
 ]
