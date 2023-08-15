e583d26ae9f35f195501df375420ba03a47f4e8c
Eyewais <80709639+Eyewais@users.noreply.github.com>
2023-08-15 23:29:49 +0500

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,8 +1,8 @@
 module.exports = [
+    { id: '1430251685847474180', user: 'flopstofreedom' },
     { id: '902926941413453824', user: 'cz_binance' },
     { id: '1484112393361891328', user: 'GCRClassic' },
     { id: '1067927682023915521', user: 'sweatystartup' },
     { id: '104281062', user: 'girdley' },
-    { id: '1658190072666021941', user: 'FrederikFeldt_' },
-    { id: '1430251685847474180', user: 'flopstofreedom' }
+    { id: '1658190072666021941', user: 'FrederikFeldt_' }
 ]
